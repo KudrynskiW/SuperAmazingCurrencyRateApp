0990bc9c28c56ad41c5851982af90d4de7a4e8f3
Wojciech Kudrynski <kudw@viessmann.com>
2025-08-02 10:13:24 +0200

    Initial setup

---
+++ SuperAmazingCurrencyRateApp/SuperAmazingCurrencyRateApp/SuperAmazingCurrencyRateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperAmazingCurrencyRateAppApp.swift
+//  SuperAmazingCurrencyRateApp
+//
+//  Created by Wojciech Kudrynski on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SuperAmazingCurrencyRateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperAmazingCurrencyRateApp/SuperAmazingCurrencyRateAppTests/SuperAmazingCurrencyRateAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  SuperAmazingCurrencyRateAppTests.swift
+//  SuperAmazingCurrencyRateAppTests
+//
+//  Created by Wojciech Kudrynski on 02/08/2025.
+//
+
+import Testing
+
+struct SuperAmazingCurrencyRateAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
